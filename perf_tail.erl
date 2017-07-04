2897fd688429ff77232f8fc0b2ebd7fc13e3c9c7
JoelDavisP <joeldavisp17195@gmail.com>
2017-07-04 23:08:49 +0530

    new commit

--- perf_tail.erl
+++ perf_tail.erl
@@ -1,4 +1,15 @@
 -module(perf_tail).
--export([perf/3]).
+-export([perf/3,perf/1]).
 
-perf(
+perf(N) ->
+  perf(N,1,0).
+
+perf(N,N,S) ->
+  N==S;
+
+perf(N,M,S) when N rem M == 0 ->
+  perf(N,M+1,S+M);
+
+perf(N,M,S) ->
+  perf(N,M+1,S).
+
